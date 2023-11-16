b696eeaaa0bcc3ed984a71bf36fc96f32af9f53f
Carsten Meyke <carsten.meyke@gmail.com>
2023-11-16 20:58:25 +0100

    EXPRESSIONS updated

--- expressions.py
+++ expressions.py
@@ -5,7 +5,7 @@ EXPRESSIONS = [
     "You appear",
     "Your skin stops tingling",
     "Your.*spell has worn off",
-    "(tells you)(?!((, 'That'll be)|(, 'I'll give you)|(.*Master)|(, 'Welcome to my bank)|(, 'Come back soon)))",
+    "(tells you)(?!((, 'That'll be)|(, 'I'll give you)|(.*Master)|(, 'Welcome to my bank)|(, 'Come back soon)|(, 'I am unable to wake)))",
 ]
 
 SPLITTER = "] "
