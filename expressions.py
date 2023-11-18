ad34687f3d127b0e1f56b6ed6ae7002523f2a681
Carsten Meyke <carsten.meyke@gmail.com>
2023-11-18 19:07:55 +0100

    EXPRESSIONS updated

--- expressions.py
+++ expressions.py
@@ -5,7 +5,7 @@ EXPRESSIONS = [
     "You appear",
     "Your skin stops tingling",
     "Your.*spell has worn off",
-    "(tells you, ')(?!((That'll be)|(I'll give you)|(Welcome to my bank)|(Come back soon)|(.*[mM]aster\\.'$)))",
+    "(tells you, ')(?!((That'll be)|(I'll give you)|(Welcome to my bank)|(Come back soon)|(You have increased your skill in)|(You are already trained to the limit of your abilities in)|(.*[mM]aster\\.'$)))",
 ]
 
 SPLITTER = "] "
